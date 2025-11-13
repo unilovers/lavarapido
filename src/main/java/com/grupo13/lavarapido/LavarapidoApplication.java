a319e214d6096c07c3cab4fcc3a6ec6f3a84108e
Gabriel Machado <gabriel.machado@edu.unifil.br>
2025-11-12 22:05:25 -0300

    :sparkles: Adicionado a base do projeto

---
+++ src/main/java/com/grupo13/lavarapido/LavarapidoApplication.java
@@ -0,0 +1,13 @@
+package com.grupo13.lavarapido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavarapidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavarapidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo13/lavarapido/LavarapidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo13.lavarapido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavarapidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
